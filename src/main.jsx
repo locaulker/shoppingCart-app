0225fd7a3e3f308846b92929a5c8e74e52664195
Lawrence Caulker <locaulker@gmail.com>
2023-10-07 13:19:10 -0400

    setup useGlobalContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import { AppProvider } from "./context"
+import "./index.css"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AppProvider>
     <App />
-  </React.StrictMode>
-);
+  </AppProvider>
+)
