38d5eb3092738b1e064fb231487c8e9c3aa91393
Lawrence Caulker <locaulker@gmail.com>
2023-10-09 18:18:44 -0400

    fetch data from external url setup

--- src/context.jsx
+++ src/context.jsx
@@ -1,7 +1,6 @@
 import { useContext, useReducer, useEffect, createContext } from "react"
 import reducer from "./reducer"
 import cartItems from "./data"
-import { getTotals } from "./utils"
 
 import {
   CLEAR_CART,
@@ -11,11 +10,13 @@ import {
   LOADING,
   DISPLAY_ITEMS
 } from "./actions"
+import { getTotals } from "./utils"
+const url = "https://www.course-api.com/react-useReducer-cart-project"
 
 const AppContext = createContext()
 
 const initialState = {
-  loading: false,
+  loading: true,
   cart: new Map(cartItems.map((item) => [item.id, item]))
 }
 
@@ -39,6 +40,15 @@ export const AppProvider = ({ children }) => {
   const decrease = (id) => {
     dispatch({ type: DECREASE, payload: { id } })
   }
+
+  const fetchData = async () => {
+    const response = await fetch(url)
+    const cart = await response.json()
+    console.log(cart)
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
     <AppContext.Provider
